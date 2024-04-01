73a542076ec2ba63f4f5ecf4950dbbefba8a7f39
Zyflx <100444522+Zyflx@users.noreply.github.com>
2024-04-01 10:57:39 -0400

    Conductor beat, step, and section shit
    hi unholy (I used your calculations lmfao)

--- game/backend/Conductor.gd
+++ game/backend/Conductor.gd
@@ -10,15 +10,38 @@ var bpm:float = 100.0:
 var crochet:float = (60.0 / bpm) * 1000.0
 var step_crochet:float = crochet * .25
 
+var beat:int = 0
+var step:int = 0
+var bar:int = 0
+
+var _beat_t:float = 0.0
+var _step_t:float = 0.0
+
 var inst:AudioStreamPlayer
 var voices:AudioStreamPlayer
 
 func _process(delta:float) -> void:
 	time += delta * 1000.0
+
+	if (time >= 0):
+		sync_music()
+		
+		if (time > (_beat_t + crochet)):
+			_beat_t += crochet
+			beat += 1
+			if (get_tree().current_scene.has_method('beat_hit')):
+				get_tree().current_scene.callv('beat_hit', [beat])
+			if (beat % 4 == 0):
+				bar += 1
+				if (get_tree().current_scene.has_method('bar_hit')):
+					get_tree().current_scene.callv('bar_hit', [bar])
+		if (time > (_step_t + step)):
+			_step_t += step_crochet
+			step += 1
+			if (get_tree().current_scene.has_method('step_hit')):
+				get_tree().current_scene.callv('step_hit', [step])
 	
-	if (time >= 0): sync_music()
-	
-func init_music(song:String, needs_voices) -> void:
+func init_music(song:String, needs_voices:bool) -> void:
 	inst = get_tree().current_scene.get_node('Inst')
 	inst.stream = load('res://assets/songs/' + song.replace(' ', '-').to_lower() + '/audio/Inst.ogg')
 	voices = get_tree().current_scene.get_node('Voices')
