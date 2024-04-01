20a8fef323dc5bb132251aabdcdf587ff5853259
Zyflx <100444522+Zyflx@users.noreply.github.com>
2024-04-01 10:58:27 -0400

    Input system now uses actions yippee

--- game/scenes/Game.gd
+++ game/scenes/Game.gd
@@ -15,7 +15,7 @@ var cpu_strums:Array[Receptor] = []
 
 var song_speed:float = 0.0
 
-var keys_arr:Array[Key] = [KEY_A, KEY_S, KEY_K, KEY_L]
+var actions_arr:Array[String] = ['note_left', 'note_down', 'note_up', 'note_right']
 
 var index:int = 0
 
@@ -43,7 +43,7 @@ func _ready() -> void:
 		else: cpu_strums.append(receptor)
 		cam_notes.add_child(receptor)
 
-func _process(_delta:float) -> void:
+func _process(delta:float) -> void:
 	while (note_data != null and note_data.size() != 0 and index != note_data.size() and note_data[index].time - Conductor.time < 1800 / song_speed):
 		if (note_data[index].time - Conductor.time > 1800 / song_speed): break
 		
@@ -54,6 +54,7 @@ func _process(_delta:float) -> void:
 		note.length = data.length
 		note.is_sustain = data.is_sustain
 		note.must_hit = data.must_hit
+		note.speed = song_speed
 		notes.append(note)
 		cam_notes.add_child(note)
 		
@@ -72,39 +73,46 @@ func _process(_delta:float) -> void:
 			if (note.can_cause_miss):
 				destroy_note(note)
 				
-func _unhandled_key_input(event) -> void:
-	var key:int = get_key_index(event.keycode)
-	if (key == -1): return
+func step_hit(step:int) -> void:
+	pass
 	
+func beat_hit(beat:int) -> void:
+	pass
+	
+func bar_hit(bar:int) -> void:
+	pass
+	
+func _unhandled_key_input(event) -> void:
+	for i in 4:
+		if (event.is_action_pressed(actions_arr[i])):
+			key_pressed(i)
+		if (event.is_action_released(actions_arr[i])):
+			key_released(i)
+		
+func key_pressed(key:int) -> void:
 	var hittable_notes:Array[Note] = notes.filter(func(n:Note):
 		return n.must_hit and n.can_hit and n.lane == key and not n.was_good_hit and not n.can_cause_miss
-	)	
-			
+	)
+	
 	hittable_notes.sort_custom(func(a:Note, b:Note): return a.time < b.time)
 
-	if (event.is_pressed() and keys_arr[key]):
-		if (hittable_notes.size() > 0):
-			var note:Note = hittable_notes[0]
+	if (hittable_notes.size() > 0):
+		var note:Note = hittable_notes[0]
+			
+		if (hittable_notes.size() > 1):
+			var behind_note:Note = hittable_notes[1]
+				
+			if (absf(behind_note.time - note.time) < 2.0):
+				destroy_note(behind_note)
+			elif (behind_note.lane == note.lane and behind_note.time < note.time):
+				note = behind_note
 					
-			if (hittable_notes.size() > 1):
-				var behind_note:Note = hittable_notes[1]
-						
-				if (absf(behind_note.time - note.time) < 2.0):
-					destroy_note(behind_note)
-				elif (behind_note.lane == note.lane and behind_note.time < note.time):
-					note = behind_note
-							
-			player_hit(note)
-		else:
-			plr_strums[key].play_anim(plr_strums[key].dir_to_name() + ' press')
+		player_hit(note)
 	else:
-		plr_strums[key].play_anim('static')
-		
-func get_key_index(key:int) -> int:
-	for i in keys_arr.size():
-		if (key == keys_arr[i]):
-			return i
-	return -1
+		plr_strums[key].play_anim(plr_strums[key].dir_to_name() + ' press')
+	
+func key_released(key:int) -> void:
+	plr_strums[key].play_anim('static')
 	
 func player_hit(note:Note) -> void:
 	plr_strums[note.lane].play_anim(plr_strums[note.lane].dir_to_name() + ' confirm')
