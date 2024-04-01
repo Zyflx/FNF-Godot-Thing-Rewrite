994a5aabaf128fa673612392f35507006954f565
ezekielxroman <ezekielroman@icluod.com>
2024-04-01 14:41:07 -0400

    extended Sprite2D for velocity and acceleration bs (thanks flixel)

---
+++ game/objects/FunkSprite.gd
@@ -0,0 +1,32 @@
+class_name FunkSprite extends Sprite2D
+
+# Sprite2D but with velocity and acceleration
+# thanks flixel
+# - Zyflx
+ 
+# the velocity of this FunkSprite. Defaults to Vector2(0, 0)
+var velocity:Vector2 = Vector2.ZERO
+# the acceleration of this FunkSprite. Defaults to Vector2(0, 0)
+var acceleration:Vector2 = Vector2.ZERO
+# whether the sprite is active or not. Defaults to false.
+# if false, all velocity and acceleration calculatons are canceled and
+# if a sprite has acceleration or velocity set to them, no effect will take place.
+var active:bool = false
+
+func _process(delta:float) -> void:
+	if (not active): pass
+	
+	var velocity_delta:Vector2 = Vector2(
+		.5 * (_calc_velocity(velocity.x, acceleration.x, delta) - velocity.x),
+		.5 * (_calc_velocity(velocity.y, acceleration.y, delta) - velocity.y)
+	)
+	
+	velocity.x += velocity_delta.x
+	position.x += velocity.x * delta
+	
+	velocity.y += velocity_delta.y
+	position.y += velocity.y * delta
+	
+# calculates the velocity of this FunkSprite.
+func _calc_velocity(_velocity:float, _acceleration:float, delta:float) -> float:
+	return _velocity + (_acceleration * delta if _acceleration != 0.0 else 0.0)
